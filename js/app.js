24460f0857801cb6ac88b850054b0acd3f0fba3c
Md. Sumon Mia <md.sumonmia.cse.iu@gmail.com>
2022-03-01 03:12:14 +0600

    error handle added

--- js/app.js
+++ js/app.js
@@ -1,32 +1,66 @@
+const parent1 = document.getElementById("parent-1");
+
 const searchField = () => {
     const searchField = document.getElementById('input-field');
     const searchFieldText = searchField.value;
-    searchField.value = ''
+    searchField.value = '';
 
+    document.getElementById('spinner').style.display = 'block';
+    parent1.textContent = '';
 
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchFieldText}`
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayPhone(data.data))
+    if (searchFieldText == '') {
+        document.getElementById('error-message').innerText = 'You did not provide input! please provide valid input';
+        parent1.textContent = '';
+    }
+
+    else {
+
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchFieldText}`
+
+             fetch(url)
+            .then(res => res.json())
+            .then(data => displayPhone(data.data))
+          //  .catch(data => displayErrorMessage(data))
+    }
 
 }
+
+
+
+// const displayErrorMessage = error => 
+// {
+//     console.log(error);
+//  document.getElementById('error-message').innerText = 'Your input is wrong! Please provide valid input.Thanks';
+// }
+
+
+
 const displayPhone = phones => {
-    //console.log(phones);
-    const parent1 = document.getElementById("parent-1");
+    console.log(phones);
+    if(phones.length==0)
+    {
+        document.getElementById('error-message').innerText = 'No Phone Found! Please check your input and try again.';
+    }
+    //document.getElementById('error-message').innerText = '';
+else
+{
+    document.getElementById('error-message').innerText = ''; 
     phones.forEach(phone => {
         console.log(phone);
-        const div = document.createElement('div');
-        div.classList.add('card');
-        div.innerHTML=`
-        <img src="${phone.image}" class="card-photo" alt="">
-        <div class="card-body">
-          <h3>${phone.phone_name}</h5>
-            <h4>${phone.brand}</h4>
-            <button onclick="" type="button" id="details-btn" class="btn btn-info fs-6">Details</button>
-         
-         `
-          parent1.appendChild(div);
-    })
+      const div = document.createElement('div');
+      div.classList.add('card');
+      div.innerHTML = `
+                 <img src="${phone.image}" class="card-photo" alt="">
+      <div class="card-body">
+                 <h3>${phone.phone_name}</h5>
+                  <h4>${phone.brand}</h4>
+                <button onclick="singlePhoneDetails('${phone.slug}')" type="button" id="details-btn" class="btn btn-info fs-6">Details</button>
+       </div>
+       `
+      parent1.appendChild(div);
+  })
 
-
+  document.getElementById('spinner').style.display = 'none';
 }
+}
+ 
