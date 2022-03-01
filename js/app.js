6e3401fd911d2a87ff7b975cfec7af20c582b27b
Md. Sumon Mia <md.sumonmia.cse.iu@gmail.com>
2022-03-01 17:12:24 +0600

    Code Comment Added

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 const parent1 = document.getElementById("parent-1");
 const parent2 = document.getElementById('parent-2');
+
+
+//----------------Search Field Code Start Here------------- --\\
+
 const searchField = () => {
     const searchField = document.getElementById('input-field');
     const searchFieldText = searchField.value;
@@ -9,99 +13,79 @@ const searchField = () => {
     parent1.textContent = '';
     parent2.textContent = '';
 
+    //Checked Search value Is Empty  or Number code Start here\\
+
     if (searchFieldText == '') {
         document.getElementById('error-message').innerText = 'You did not provide input! please provide valid input.';
         parent1.textContent = '';
         parent2.textContent = '';
     }
-     else if (Number.isInteger(parseInt(searchFieldText))) {
+    else if (Number.isInteger(parseInt(searchFieldText))) {
         document.getElementById('error-message').innerText = 'Do not allow number type value! please provide valid input.';
         parent1.textContent = '';
         parent2.textContent = '';
     }
-
     else {
-
         const url = `https://openapi.programming-hero.com/api/phones?search=${searchFieldText}`
-
         fetch(url)
             .then(res => res.json())
             .then(data => displayPhone(data.data))
-
     }
 
+    //Checked Search value Is Empty  or Number code End here\\
 }
+
+//----------------Search Field Code End Here------------- --\\
+
+
+
+//----------------Search Result Code start Here-----------\\
+
 const displayPhone = phones => {
 
+    //Checked Search value Is array element  or not code Start here\\
     if (phones.length == 0) {
         document.getElementById('error-message').innerText = 'No Phone Found! Please check your input and try again.';
     }
-
     else {
-
         document.getElementById('error-message').innerText = '';
         const deviceLimit = phones.slice(0, 20);
         deviceLimit.forEach(phone => {
-            //  console.log(phone);
             const div = document.createElement('div');
             div.classList.add('card');
             div.innerHTML = `
-                 <img src="${phone.image}" class="card-photo" alt="">
-      <div class="card-body">
-                 <h3>${phone.phone_name}</h5>
-                  <h4>${phone.brand}</h4>
-                <button onclick="singlePhoneDetails('${phone.slug}')" type="button" id="details-btn" class="btn btn-info fs-6">Details</button>
-       </div>
+          <img src="${phone.image}" class="card-photo" alt="">
+          <div class="card-body">
+             <h3>${phone.phone_name}</h5>
+             <h4>${phone.brand}</h4>
+             <button onclick="singlePhoneDetails('${phone.slug}')" type="button" id="details-btn" class="btn btn-info fs-6 fw-bold">Details</button>
+          </div>
        `
             parent1.appendChild(div);
         })
-
         document.getElementById('spinner').style.display = 'none';
     }
+    //Checked Search value Is array element  or not code End here\\
 }
 
+//----------------Search Result Code End Here-----------\\
 
 
 
+//----------------Single phone API loaded Code Start Here-----------\\
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const singlePhoneDetails = id => {
-    const url = ` https://openapi.programming-hero.com/api/phone/${id}`
+const singlePhoneDetails = productsId => {
+    const url = ` https://openapi.programming-hero.com/api/phone/${productsId}`
     fetch(url)
         .then(res => res.json())
         .then(data => displaySinglePhoneDetails(data.data))
 }
 
-//const releaseDateValue='not found'
+//----------------Single phone API loaded Code End Here-----------\\
+
+
+
+//------------------display Single Phone Details Code Start Here----------------\\
 
 const displaySinglePhoneDetails = phone => {
     parent2.textContent = '';
@@ -113,25 +97,25 @@ const displaySinglePhoneDetails = phone => {
                       <img src="${phone.image}" alt="">
                   </div>
     
-                <div class="col-md-8 bg-dark text-white rounded shadow-lg">
-                        <h3>Name: ${phone.name}</h3>
-                        <h5>ReleaseDate: ${phone.releaseDate ? phone.releaseDate : `Not Found! Coming soon.`}</h5>
-                        <h5>Brand: ${phone.brand}</h5>
-                        <h5>DisplaySize: ${phone.mainFeatures.displaySize}</h5>
-                        <h5>Storage: ${phone.mainFeatures.storage}</h5>
-                        <h5>ChipSet: ${phone.mainFeatures.chipSet}</h5>
-                        <h5>Memory: ${phone.mainFeatures.memory}</h5>
-                        <h5>Sensors: ${phone.mainFeatures.sensors.slice()}</h5>
-                        <p>WLAN: ${phone.others.WLAN}</p>
-                        <P>Bluetooth: ${phone.others.Bluetooth}</P>
-                        <P>GPS: ${phone.others.GPS}</P>
-                        <P>NFC: ${phone.others.NFC}</P>
-                        <P>Radio: ${phone.others.Radio}</P>
-                        <p>USB: ${phone.others.USB}</p>
-                </div>
-         </div>
+                <div class="col-md-8 bg-dark text-white shadow-lg details-card">
+                        <h3><u>Name</u>: ${phone.name}</h3>
+                        <h5><u>ReleaseDate</u>: ${phone.releaseDate ? phone.releaseDate : `Not Found! Coming soon.`}</h5>
+                        <h5><u>Brand</u>: ${phone.brand}</h5>
+                        <h5><u>DisplaySize</u>: ${phone.mainFeatures.displaySize}</h5>
+                        <h5><u>Storage</u>: ${phone.mainFeatures.storage}</h5>
+                        <h5><u>ChipSet</u>: ${phone.mainFeatures.chipSet}</h5>
+                        <h5><u>Memory</u>: ${phone.mainFeatures.memory}</h5>
+                        <h5><u>Sensors</u>: ${phone.mainFeatures.sensors.slice()}</h5>
+                        <p><u>WLAN</u>: ${phone.others.WLAN}</p>
+                        <P><u>Bluetooth</u>: ${phone.others.Bluetooth}</P>
+                        <P><u>GPS</u>: ${phone.others.GPS}</P>
+                        <P><u>NFC</u>: ${phone.others.NFC}</P>
+                        <P><u>Radio</u>: ${phone.others.Radio}</P>
+                        <p><u>USB</u>: ${phone.others.USB}</p>
+                        </div>
+                 </div>
     `
     parent2.appendChild(div);
 }
 
-
+//------------------display Single Phone Details Code Start Here----------------\\
